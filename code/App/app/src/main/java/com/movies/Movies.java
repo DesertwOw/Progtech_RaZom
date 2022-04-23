0a20b083942ab1ebb71f0acb3cd25fcc91de23e7
Rados Kevin <rkevin99@gmail.com>
2022-04-23 21:52:32 +0200

    Created the Add Movie upload function (has problems)

--- code/App/app/src/main/java/com/movies/Movies.java
+++ code/App/app/src/main/java/com/movies/Movies.java
@@ -2,20 +2,21 @@ package com.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Movies extends AppCompatActivity {
 
     Studio universalPictures;
     ActionMovie movie;
-    TextView movieName;
-    TextView length;
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +32,42 @@ public class Movies extends AppCompatActivity {
         addmoviebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                movie = universalPictures.createActionMovie(movieName.getText().toString(),Integer.parseInt(length.getText().toString()));
+                TextView movieName =(TextView) findViewById(R.id.moviename);
+                TextView length = (TextView) findViewById(R.id.length);
+                if(!movieName.equals("") && !length.equals("")) {
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            movie = universalPictures.createActionMovie(movieName.getText().toString(),Integer.parseInt(length.getText().toString()));
+                            String[] field = new String[5];
+                            field[0] = "moviename";
+                            field[1] = "length";
+                            String[] data = new String[5];
+                            data[0] = movieName.toString();
+                            data[1] = length.toString();
+                            PutData putData = new PutData("http://192.168.1.199/Mobile_API/Add_movie.php", "POST", field, data);
+                            //cmd -> ipconfig -> ipv4 address
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Movie Added successfully!")){
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(), result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"All fields are required!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
