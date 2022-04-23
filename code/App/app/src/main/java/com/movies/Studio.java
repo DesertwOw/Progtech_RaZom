44014d8d814319ee94cc3dfd1515d5ac325f4eb9
Rados Kevin <rkevin99@gmail.com>
2022-04-23 18:18:55 +0200

    Created the base of the AbstractFactoryMethod

---
+++ code/App/app/src/main/java/com/movies/Studio.java
@@ -0,0 +1,7 @@
+package com.movies;
+
+public abstract class Studio {
+
+    public abstract ActionMovie createActionMovie(String name, int length);
+    public abstract ComedyMovie createComedyMovie();
+}
