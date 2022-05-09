78135a3bcc15f7ae4fd23a9d201a2cb9dfba30e4
zbalint00 <zbalint00@gmail.com>
2022-05-09 02:48:27 +0200

    Container class in order to save movies in 1 list

---
+++ code/App/app/src/main/java/com/movies/Container.java
@@ -0,0 +1,9 @@
+package com.movies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Container {
+    //TODO minden indításnál feltölteni az adatbázisból
+    public static List<MovieBase> Movies = new ArrayList<MovieBase>();
+}
