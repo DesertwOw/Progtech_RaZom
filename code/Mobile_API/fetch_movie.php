ea5e80cb0ea2e34d0258a91657eff54a597addc9
Rados Kevin <rkevin99@gmail.com>
2022-05-14 23:22:39 +0200

    Uploaded the fully working fetcher api function

---
+++ code/Mobile_API/fetch_movie.php
@@ -0,0 +1,27 @@
+<?php
+
+$host='localhost';
+$username='root';
+$pwd='';
+$db='movies';
+
+$con=mysqli_connect($host,$username,$pwd,$db) or die('Unable to connect');
+
+if(mysqli_connect_error())
+{
+    echo "Failed to connect";
+}
+
+$query=mysqli_query($con,"SELECT * from movie");
+
+if($query)
+{
+    while($row=mysqli_fetch_array($query))
+    {
+        $flag[] =$row;
+    }
+    print(json_encode($flag));
+}
+
+mysqli_close($con);
+?>
