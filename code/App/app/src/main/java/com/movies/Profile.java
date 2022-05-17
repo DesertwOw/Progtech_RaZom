e9a646eb22d9bb5851e7e5af375e7918da7ba612
zbalint00 <zbalint00@gmail.com>
2022-05-17 15:07:56 +0200

    Show the logged in user datas

--- code/App/app/src/main/java/com/movies/Profile.java
+++ code/App/app/src/main/java/com/movies/Profile.java
@@ -7,13 +7,34 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Profile extends AppCompatActivity {
+
+    String urladdress = Config.showURL + "getProfile.php";
+    String[] Userid;
+    String[] Username;
+    String[] Firstname;
+    String[] Lastname;
+    String[] Email;
+    BufferedInputStream is;
+    String line = null;
+    String result = null;
 
     final String MODIFYBTN = "Button created";
     final String BACKBTN = "Button created";
@@ -22,6 +43,12 @@ public class Profile extends AppCompatActivity {
     final String UPLOADBAD = "Bad data upload";
     final String GOODREQ = "Good request handled";
     final String BADREQ = "Bad request handled";
+    final String FETCHG = "Fetch request good";
+    final String FETCHB = "Fetch request failed";
+    final String GETREQ = "Get request started";
+    final String BADGETREQ = "Get request failed";
+    final String JSONENCODE = "Json encoded succesfuly";
+    final String JSONFAIL = "Failed the encoding";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +62,14 @@ public class Profile extends AppCompatActivity {
         TextView first_name = (TextView) findViewById(R.id.first_name);
         TextView last_name = (TextView) findViewById(R.id.last_name);
         TextView email = (TextView) findViewById(R.id.email);
+        collectData();
+        getDatas();
+        userid.setText(User.userid);
+        username.setText(User.username);
+        passwd.setText(User.password);
+        first_name.setText(User.firstname);
+        last_name.setText(User.lastname);
+        email.setText(User.email);
 
         MaterialButton modifybtn = (MaterialButton) findViewById(R.id.modifybtn);
         Log.i(MODIFYBTN,"Modify button created successfully!");
@@ -107,5 +142,71 @@ public class Profile extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    private void collectData()
+    {
+        try {
+            URL url = new URL(urladdress);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            is = new BufferedInputStream(con.getInputStream());
+            Log.i(GETREQ,"Request started");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Log.e(BADGETREQ,"Request failed");
+        }
+        try
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            while((line=br.readLine()) != null){
+                sb.append(line+"\n");
+            }
+            is.close();
+            result=sb.toString();
+            Log.i(FETCHG,"Fetch finished");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Log.e(FETCHB,"Fetch failed");
+        }
+
+        try{
+            JSONArray ja = new JSONArray(result);
+            JSONObject jo = null;
+            Userid = new String[ja.length()];
+            Username = new String[ja.length()];
+            Firstname = new String[ja.length()];
+            Lastname = new String[ja.length()];
+            Email = new String[ja.length()];
+
+            for(int i = 0; i<=ja.length(); i++){
+                jo = ja.getJSONObject(i);
+                Userid[i] = jo.getString("id");
+                Username[i] = jo.getString("username");
+                Firstname[i] = jo.getString("first_name");
+                Lastname[i] = jo.getString("last_name");
+                Email[i] = jo.getString("email");
+
+                Log.i(JSONENCODE,"Data loaded");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e(JSONFAIL,"Data load failed");
+        }
+    }
+
+    private void getDatas(){
+        for(int i = 0; i < Userid.length; i++){
+            if(User.username.equals(Username[i])){
+                User.userid = Userid[i];
+                User.firstname = Firstname[i];
+                User.lastname = Lastname[i];
+                User.email = Email[i];
+            }
+        }
     }
 }
