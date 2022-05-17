2a63227d095752494d8ab1bae6eab94965582a83
zbalint00 <zbalint00@gmail.com>
2022-05-17 15:07:21 +0200

    Make User class in order to store user datas

---
+++ code/App/app/src/main/java/com/movies/User.java
@@ -0,0 +1,10 @@
+package com.movies;
+
+ public class User {
+    static protected String userid;
+    static protected String username;
+    static protected String password;
+    static protected String firstname;
+    static protected String lastname;
+    static protected String email;
+}
