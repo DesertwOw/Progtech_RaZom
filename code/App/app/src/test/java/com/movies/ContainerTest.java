d7b097f3b158288d0cd2bf321288c3f197cdf4fe
zbalint00 <zbalint00@gmail.com>
2022-05-18 02:39:57 +0200

    Made Unti tests

---
+++ code/App/app/src/test/java/com/movies/ContainerTest.java
@@ -0,0 +1,57 @@
+package com.movies;
+
+import static org.junit.Assert.*;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ContainerTest {
+
+    @Test
+    public void fillContainer() {
+        Container.Movies.clear();
+         String studio = null;
+         String title = null;
+         String category = null;
+         String length = null;
+         Container.collectData();
+         Container.fillContainer(studio, title, category, length);
+
+        for(int i = 0; i < Container.Movies.size(); i++){
+            System.out.println(Container.Movies.get(i).toString());
+        }
+        Container.Movies.clear();
+
+    }
+
+    @Test
+    public void ContainerListTest(){
+        Container.Movies.clear();
+        ActionMovie action = new ActionMovie("Universal Pictures", "Action", "Halálos Iramban", 120);
+        ActionMovie action1 = new ActionMovie("Universal Pictures", "Action", "Harry Potter", 150);
+        ActionMovie action2 = new ActionMovie("Marvel", "Action", "Pókember", 127);
+        ComedyMovie comedy = new ComedyMovie("Pixar", "Comedy", "Szörny Rt", 100);
+        ComedyMovie comedy1 = new ComedyMovie("Pixar", "Comedy", "Toy Story", 97);
+        ComedyMovie comedy2 = new ComedyMovie("Pixar", "Comedy", "Fel", 86);
+        Container.Movies.add(action);
+        Container.Movies.add(action1);
+        Container.Movies.add(action2);
+        Container.Movies.add(comedy);
+        Container.Movies.add(comedy1);
+        Container.Movies.add(comedy2);
+        for(int i = 0; i < Container.Movies.size(); i++){
+            System.out.println(Container.Movies.get(i).toString());
+        }
+        Container.Movies.clear();
+    }
+}
---
+++ code/App/app/src/test/java/com/movies/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.movies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Test
+    public void LoginTest(){
+        String username = "Unit";
+        String passwd = "Teszt";
+        // MainActivity main = new MainActivity();
+        // main.Login(username, passwd);
+
+
+    }
+
+}
