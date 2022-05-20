d87808eb13d6427e12737d14ecb1ba25b8735b06
zbalint00 <zbalint00@gmail.com>
2022-05-20 04:03:41 +0200

    Made Enum folder for enums

---
+++ code/App/app/src/main/java/com/movies/Enums/Gender.java
@@ -0,0 +1,5 @@
+package com.movies.Enums;
+
+public enum Gender {
+    Man, Woman
+}
---
+++ code/App/app/src/main/java/com/movies/Enums/Movie_playedRole.java
@@ -0,0 +1,5 @@
+package com.movies.Enums;
+
+public enum Movie_playedRole {
+    Main_Character, Guest_Star, Background_Actor, Supporting_Actor
+}
